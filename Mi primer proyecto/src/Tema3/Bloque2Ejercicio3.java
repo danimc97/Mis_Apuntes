c18bc0e3ba9a4ec82f1d5c00593ad5a70ef0c2cb
diurno <diurno@pc-aula>
2018-10-08 13:13:36 +0200

    Ultimo

--- Mi primer proyecto/src/Tema3/Bloque2Ejercicio3.java
+++ Mi primer proyecto/src/Tema3/Bloque2Ejercicio3.java
@@ -5,29 +5,14 @@ import javax.swing.JOptionPane;
 public class Bloque2Ejercicio3 {
 
 	public static void main(String[] args) {
-		
-		int num=Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero: "));
-		int division=0;
-		int num2=num;
-		
-		if (num>100) {
-		System.out.println("Error");	
-		}
-		else {
-			for (int i=0;num2>=1;i++) {
-				
-				division=num%num2;
-				
-				if (division==0) {
-					
-					System.out.println(num2);
-					
+		int mul=0;
+		int num=Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero: "));
+		for (int o=1;mul<100;o++) {
+				mul=num*o;
+				if (mul<100) {
+				System.out.println(num+"x"+o+"="+mul);
 				}
-				num2=num2-1;
-				
 			}
-
-		}
+		}	
 	}
 
-}
--- Mi primer proyecto/src/Tema3/Bloque3Ejercicio6.java
+++ Mi primer proyecto/src/Tema3/Bloque3Ejercicio6.java
@@ -8,27 +8,17 @@ public class Bloque3Ejercicio6 {
 		
 		int num=1;
 		int mul=0;
-		
 		for (int i=0;num!=0;i++) {
-			
 			num=Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero: "));
-			
 			if(num==0) {
 				System.out.println("Error");
 			}
 			else {
-			
 				for (int o=0;o<=10;o++) {
-					
 					mul=num*o;
-					System.out.println(num+"x"+o+"="+mul);
-					
+					System.out.println(num+"x"+o+"="+mul);		
 				}
-			
-		}
-		
+		}	
 	}
-
 	}
-
 }
