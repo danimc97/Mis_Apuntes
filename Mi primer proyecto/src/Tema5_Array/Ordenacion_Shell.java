ece8e4cde814799b2f56008630e556823fa363d4
dani <dani@dani-VirtualBox>
2018-10-18 00:51:37 +0200

    Noche

--- Mi primer proyecto/src/Tema5_Array/Ordenacion_Shell.java
+++ Mi primer proyecto/src/Tema5_Array/Ordenacion_Shell.java
@@ -6,13 +6,13 @@ public class Ordenacion_Shell {
 
 	public static void main(String[] args) {
 		
-		int array[]= new int[6];
+		int array[]= new int[10];
 		int num=0;
 		int i=0;
 		int j=0;
 		int aux;
 		
-		for (i=0;i<array.length;i++) {
+		for (i=0;i<=4;i++) {
 			num=(int) Math.round(Math.random()*100);
 			array[i]=num;
 			System.out.print(array[i]+" ");
@@ -20,20 +20,30 @@ public class Ordenacion_Shell {
 		
 		System.out.println();
 		
-		for (i=1;i<array.length;i++) {
+		for (i=5;i<10;i++) {
+			num=(int) Math.round(Math.random()*100);
+			array[i]=num;
+			System.out.print(array[i]+" ");
+		}
+		
+		
+		System.out.println();
+		
+		for (i=0;i<=4;i++) {
 			
-			aux=array[i];
-			
-			for (j=i-1;j>=0&&array[j]>aux;j--) {
+			for (j=1+i;j<=4;j++) {
 				
-				array[j+1]=array[j];
-				array[j]=aux;
-				
+				if (array[i]>array[j]) {	
+					
+					aux=array[i];
+					array[i]=array[j];
+					array[j]=aux;	
+				}
 			}
 			
 		}
 		System.out.println();
-		for (i=0;i<array.length;i++) {
+		for (i=0;i<=4;i++) {
 			System.out.print(array[i]+" ");
 		}
 	}
