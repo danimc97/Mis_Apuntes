4dab6d7dc0a5570d4d152591f29bdde7128e6574
diurno <diurno@pc-aula>
2018-11-09 15:01:10 +0100

    Clase

--- Mi primer proyecto/src/arbolito/Arbol.java
+++ Mi primer proyecto/src/arbolito/Arbol.java
@@ -2,4 +2,16 @@ package arbolito;
 
 public class Arbol {
 
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
 }
