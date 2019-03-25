3f0345c2ba71a1782054b5a4fff2af3cc66031a3
diurno <diurno@pc-aula>
2019-03-25 14:56:31 +0100

    Clase

--- Mi primer proyecto/src/swing_ejercicios/VentanaPrincipal.java
+++ Mi primer proyecto/src/swing_ejercicios/VentanaPrincipal.java
@@ -11,15 +11,23 @@ import java.sql.SQLException;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 
+
 public class VentanaPrincipal extends JFrame {
 
+	
+	String combo[] = new String[] {"Ciudad1","Ciudad2"};
+	private static char digitosHexadecimales[] = new char[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+	
 	
 	public VentanaPrincipal () {
 		super ("Ventana");
@@ -30,20 +38,24 @@ public class VentanaPrincipal extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
-		// Construcci�n elementos b�sicos sobre el ContentPanel
-		this.setContentPane(getPanelPrincipal());
+		JTabbedPane tabbedPane = new JTabbedPane();
+		
+		tabbedPane.add("01", getPanelTab01());
+		tabbedPane.add("02", getPanelTab02());
+		
+		this.setContentPane(tabbedPane);
 		
 	}
 	
 	
-	private JTabbedPane getPanelPrincipal () {
-		
-		JTabbedPane tabbedPane = new JTabbedPane();
+	private JPanel getPanelTab01 () {	
 		
 		
 		JPanel panel = new JPanel();
 		//panel.setBackground(Color.RED);
 		panel.setLayout(new FlowLayout());
+		
+		
 		
 		JLabel jlb = new JLabel();
 		
@@ -56,11 +68,15 @@ public class VentanaPrincipal extends JFrame {
 		jtf.setColumns(40);
 		jtf.setText("");
 		
-		panel.add(jtf);
+		panel.add(jtf); 
 		
 		JButton jbt = new JButton("Aceptar");
 		
 		JCheckBox jcb = new JCheckBox("Soy un checkbox");
+		
+		/**
+		 * 
+		 */
 		
 		jbt.addActionListener(new ActionListener() {
 			
@@ -70,6 +86,14 @@ public class VentanaPrincipal extends JFrame {
 				String str = ("Contenido del JTexfield: "+ jtf.getText() + " y el valor del check es "+jcb.isSelected());
 				JOptionPane.showMessageDialog(null, str);
 				
+			}
+		});
+		
+		jcb.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {				
+				JOptionPane.showMessageDialog(null, "Ha cambiado el estado del checkbox a "+jcb.isSelected());				
 			}
 		});
 		
@@ -86,9 +110,99 @@ public class VentanaPrincipal extends JFrame {
 		 * 
 		 */
 		
-		tabbedPane.add("01", panel);
+		return panel;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	
+	private JPanel getPanelTab02 () {	
 		
-		return tabbedPane;
+		
+		JPanel panel = new JPanel();
+		//panel.setBackground(Color.RED);
+		panel.setLayout(new FlowLayout());
+		
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		
+		JLabel jlb = new JLabel();
+		
+		jlb.setText("Usuario: ");
+		
+		panel.add(jlb);
+		
+		JTextField jtf = new JTextField();
+		
+		jtf.setColumns(40);
+		jtf.setText("");
+		
+		panel.add(jtf);
+		
+		
+		/**
+		 * 
+		 * 
+		 * 
+		 */
+		
+		JLabel jlbp = new JLabel();
+		
+		jlbp.setText("Contraseña: ");
+		
+		panel.add(jlbp);
+		
+		JPasswordField jpf = new JPasswordField();
+		jpf.setColumns(40);
+		panel.add(jpf);
+		
+		/**
+		 * 
+		 * 
+		 */
+		
+		JComboBox jcb = new JComboBox (combo);
+		panel.add(jcb);
+		
+		/**
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		JList jl = new JList(combo);
+		panel.add(jl);
+		
+		/**
+		 * 
+		 * 
+		 * 
+		 */
+		
+		JButton jb = new JButton("Aceptar");
+		panel.add(jb);
+		
+		jb.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				panel.setBackground(Color.decode(getColorAzar()));
+			}
+		});
+		
+		jcb.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				JOptionPane.showMessageDialog(null, "El valor ha cambiado a "+jcb.getSelectedItem());
+				
+			}
+		});
+		
+		return panel;
 	}
 	
 	
@@ -97,6 +211,17 @@ public class VentanaPrincipal extends JFrame {
 	private void setDimensionesBasicas () {
 		this.setExtendedState(JFrame.NORMAL);
 		this.setBounds(0, 0, 800, 600);
+	}
+	
+	
+	
+	
+	private static String getColorAzar () {
+		StringBuffer sb = new StringBuffer ("#");
+		for (int i = 0; i < 6; i++) {
+			sb.append(digitosHexadecimales[Utils.obtenerNumeroAzar(0, digitosHexadecimales.length-1)]);
+		}
+		return sb.toString();
 	}
 	
 }
