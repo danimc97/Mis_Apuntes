0520bb0a480841a08d171ddf43db60c9daa27fe7
diurno <diurno@pc-aula>
2019-03-29 14:43:41 +0100

    Clase

---
+++ Mi primer proyecto/src/tutorialJava/capitulo8_AWT_SWING/ejercicios/ejercicio03_GestionEntidadCoche/modelo/ImposibleConectarException.java
@@ -0,0 +1,10 @@
+package tutorialJava.capitulo8_AWT_SWING.ejercicios.ejercicio03_GestionEntidadCoche.modelo;
+
+public class ImposibleConectarException extends Exception {
+
+	public ImposibleConectarException(String message) {
+		super(message);
+	}
+
+	
+}
